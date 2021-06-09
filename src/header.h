b606d674bd1991e90d1f7123205bcb36778fe73f
m1gle <78843923+m1gle@users.noreply.github.com>
2021-06-09 17:55:30 +0300

    Update header.h

--- src/header.h
+++ src/header.h
@@ -1,1 +1,21 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <sstream>
+#include <regex>
 
+using std::cout;
+using std::endl;
+using std::string;
+using std::map;
+using std::regex;
+using std::multimap;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::setw;
+using std::left;
+using std::right;
